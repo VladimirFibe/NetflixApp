fb1fffb0738c3c8a25818a904e094cf65bcea043
Vladimir Fibe <motiw@icloud.com>
2024-04-26 21:32:24 +0500

    createHeroSection

--- NetflixApp/ViewController.swift
+++ NetflixApp/ViewController.swift
@@ -42,8 +42,25 @@ class ViewController: UIViewController {
         snapshot.appendItems([Movie(), Movie(), Movie(), Movie(), Movie()], toSection: .movie)
         dataSorce.apply(snapshot)
     }
+}
 
-    func createLayout() -> UICollectionViewLayout {
+extension ViewController {
+    private func createHeroSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .absolute(500))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                       subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        return section
+    }
+    
+    private func createMovieSection() -> NSCollectionLayoutSection {
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
                                               heightDimension: .fractionalHeight(1.0))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
@@ -55,7 +72,17 @@ class ViewController: UIViewController {
         let section = NSCollectionLayoutSection(group: group)
         section.orthogonalScrollingBehavior = .continuous
         section.interGroupSpacing = 8
-        let layout = UICollectionViewCompositionalLayout(section: section)
+        return section
+    }
+ 
+    func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout{ index, environment in
+            if index == 0 {
+                self.createHeroSection()
+            } else {
+                self.createMovieSection()
+            }
+        }
         return layout
     }
 }
