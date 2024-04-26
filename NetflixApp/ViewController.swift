f17514fe0e28415638f20268a63f9a2fad8041af
Vladimir Fibe <motiw@icloud.com>
2024-04-26 20:12:45 +0500

    Initial Commit

---
+++ NetflixApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixApp
+//
+//  Created by FibeApp on 26.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
